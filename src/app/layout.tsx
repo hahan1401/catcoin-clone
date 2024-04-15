e1290e234b99b893a5df6249cecec0b2d14a5e72
hahan <hanvietha141@gmail.com>
2024-04-15 20:24:21 +0700

    (*) init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+config.autoAddCss = false;
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-bg`}>{children}</body>
     </html>
   );
 }
