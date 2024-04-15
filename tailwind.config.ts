0d4abd91d38799af8c617dcab22c99fbfad20c8e
hahan <hanvietha141@gmail.com>
2024-04-15 20:55:50 +0700

    (+) init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
       },
       spacing: {
         2.5: "0.625rem",
+        4.5: "1.125rem",
       },
       fontSize: {
         lg: ["1.125rem", { lineHeight: "1.3125rem" }],
